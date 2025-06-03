8339f0dc123baaed8efa475cb2db150d69922ab1
Hakan <m.hakanytc@gmail.com>
2025-06-03 08:19:29 -0400

    Initial commit

---
+++ Modules/Blueprint/module_c.py
@@ -0,0 +1,7 @@
+MODULE_NAME = "Your Module Name"
+MODULE_DESCRIPTION = "Description of your module"
+MODULE_ICON = "path/to/icon.png"  # Optional
+
+def module_function():
+    # Function that runs when the icon is clicked
+    print('module_b')
